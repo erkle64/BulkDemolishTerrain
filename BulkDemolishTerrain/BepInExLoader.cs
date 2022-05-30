49a64ac45229bb14bf2a2dcaa241ee2415c12d79
erkle64 <erkle64@gmail.com>
2022-05-31 03:10:02 +1200

    Initial source code commit

---
+++ BulkDemolishTerrain/BepInExLoader.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnhollowerRuntimeLib;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace BulkDemolishTerrain
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class BepInExLoader : BepInEx.IL2CPP.BasePlugin
+    {
+        public const string
+            MODNAME = "BulkDemolishTerrain",
+            AUTHOR = "erkle64",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0";
+
+        public static BepInEx.Logging.ManualLogSource log;
+
+        public BepInExLoader()
+        {
+            log = Log;
+        }
+
+        public override void Load()
+        {
+            log.LogMessage("Registering PluginComponent in Il2Cpp");
+
+            try
+            {
+                ClassInjector.RegisterTypeInIl2Cpp<PluginComponent>();
+
+                var go = new GameObject("Erkle64_BulkDemolishTerrain_PluginObject");
+                go.AddComponent<PluginComponent>();
+                Object.DontDestroyOnLoad(go);
+            }
+            catch
+            {
+                log.LogError("FAILED to Register Il2Cpp Type: PluginComponent!");
+            }
+
+            try
+            {
+                var harmony = new Harmony(GUID);
+
+                var original = AccessTools.Method(typeof(Character.BulkDemolishBuildingEvent), "processEvent");
+                var post = AccessTools.Method(typeof(PluginComponent), "processBulkDemolishBuildingEvent");
+                harmony.Patch(original, postfix: new HarmonyMethod(post));
+
+                original = AccessTools.Method(typeof(InputProxy), "Update");
+                post = AccessTools.Method(typeof(PluginComponent), "Update");
+                harmony.Patch(original, postfix: new HarmonyMethod(post));
+            }
+            catch
+            {
+                log.LogError("Harmony - FAILED to Apply Patch's!");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# BulkDemolishTerrain for [Foundry](https://www.foundry-game.com/)
+
+A mod to add a quick craft button.  It will craft the currently selected item in the hot bar.
+
+#### Installation
+
+1. **Download**  
+Download [BulkDemolishTerrain](https://github.com/erkle64/BulkDemolishTerrain/releases).
+
+2. **Install**  
+Extract everything inside the zip into your Foundry folder.
+
+#### Usage
+
+* Just use the bulk demolish mode (ctrl-Z) on terrain and make sure to have enough inventory space for it.
+
+#### Compatibility
+
+Unlikely to be compatible with other mods (except FastMiner and BulkDemolishTerrain).
+
+Probably only works in single player.
